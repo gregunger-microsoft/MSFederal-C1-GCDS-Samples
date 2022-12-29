fdcac0d64be483c2f4062ca7696437171eba6358
Dave Willis <dwillis50@gmail.com>
2022-12-29 12:24:38 -0500

    Initial 4.72 app

---
+++ OIDC-Samples/DotnetFramework472Sample/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DotnetFramework472Sample.MvcApplication" Language="C#" %>
---
+++ OIDC-Samples/DotnetFramework472Sample/Startup.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+namespace DotnetFramework472Sample
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
